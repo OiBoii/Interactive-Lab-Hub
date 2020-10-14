8d7108e671e82ec867be41202df2ef537fadb9e8
Omer <oi38@cornell.edu>
2020-10-13 23:49:47 -0400

    Update accelerometer.ino

--- Lab4/accelerometer.ino
+++ Lab4/accelerometer.ino
@@ -1,27 +1,66 @@
-
 // Basic demo for accelerometer readings from Adafruit LIS3DH
 
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-// Used for software SPI
-#define LIS3DH_CLK 13
-#define LIS3DH_MISO 12
-#define LIS3DH_MOSI 11
-// Used for hardware & software SPI
-#define LIS3DH_CS 10
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+
+//// Used for software SPI
+//#define LIS3DH_CLK 13
+//#define LIS3DH_MISO 12
+//#define LIS3DH_MOSI 11
+//// Used for hardware & software SPI
+//#define LIS3DH_CS 10
 
 // software SPI
-//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS, LIS3DH_MOSI, LIS3DH_MISO, LIS3DH_CLK);
+// Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS, LIS3DH_MOSI, LIS3DH_MISO, LIS3DH_CLK);
 // hardware SPI
-//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS);
+// Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS);
 // I2C
 Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 
+//void writeToOLED(string info, float num) {
+//  //display.clearDisplay();
+//
+//  display.setTextSize(2);
+//  display.setTextColor(SSD1306_WHITE);
+//  //display.setCursor(0,0);             // Start at top-left corner
+//  display.print(info);
+//
+//  display.display();
+//}
+
 void setup(void) {
-  Serial.begin(115200);
+  Serial.begin(9600);
+
+  //OLED screen
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  //Serial stuff
   while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
 
   Serial.println("LIS3DH test!");
@@ -30,6 +69,7 @@ void setup(void) {
     Serial.println("Couldnt start");
     while (1) yield();
   }
+
   Serial.println("LIS3DH found!");
 
   // lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
@@ -55,23 +95,31 @@ void setup(void) {
 }
 
 void loop() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  
   lis.read();      // get X Y and Z data at once
-  // Then print out the raw data
-  Serial.print("X:  "); Serial.print(lis.x);
-  Serial.print("  \tY:  "); Serial.print(lis.y);
-  Serial.print("  \tZ:  "); Serial.print(lis.z);
+
+  display.print("X: "); display.print(lis.x);
+  display.print(" Y: "); display.print(lis.y);
+  display.print(" Z: "); display.print(lis.z);
+
+  display.println();
 
   /* Or....get a new sensor event, normalized */
   sensors_event_t event;
   lis.getEvent(&event);
 
-  /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("\t\tX: "); Serial.print(event.acceleration.x);
-  Serial.print(" \tY: "); Serial.print(event.acceleration.y);
-  Serial.print(" \tZ: "); Serial.print(event.acceleration.z);
-  Serial.println(" m/s^2 ");
+  display.print("X: "); display.print(event.acceleration.x);
+  display.print(" Y: "); display.print(event.acceleration.y);
+  display.print(" Z: "); display.print(event.acceleration.z);
+  display.println(" m/s^2 ");
 
-  Serial.println();
+  display.println();
+
+  display.display();
 
   delay(200);
 }
